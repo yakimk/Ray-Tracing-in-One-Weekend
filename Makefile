aa922b423ab5c057769927e39568e355ca6cd8cb
Rio <yakimenko.kir@gmail.com>
2022-12-28 20:20:47 +0100

    Real first commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+01: ./src/01-de-graphiq-que-programme.cpp 
+	clang++ ./src/01-de-graphiq-que-programme.cpp -g  -Wall -o ./build/01.exe
+	./build/01.exe > ./images/01.ppm
---
+++ src/01-de-graphiq-que-programme.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cstdlib>
+
+int main(){
+    int r,g,b;
+    const int height = 256;
+    const int width = 256;
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+    for (int j=height-1; j>=0;j--){
+        std::cerr<<"\rLines remaining: "<<j<<" "<<std::flush;
+        for (int i=0;i<width; i++){
+            r= rand()%255;
+            g= rand()%255;
+            b= rand()%255;
+            int ir = static_cast<int>(r);
+            int ig = static_cast<int>(g);
+            int ib = static_cast<int>(b);
+            std::cout<<ir<<' '<<ig<<' '<<ib<<'\n';
+        }
+    }
+    std::cerr<<"\nSuccess. \n";
+}
