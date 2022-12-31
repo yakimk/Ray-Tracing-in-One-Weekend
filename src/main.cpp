38c8bae69235e9de60c3e66da8fea28be047f1d1
Rio <yakimenko.kir@gmail.com>
2022-12-31 14:41:09 +0100

    Minor

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main() {
 
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100,-100), 100));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
