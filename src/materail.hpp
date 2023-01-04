5f98f04715aa5f22a45f0260befeeca06ea62e76
Rio <yakimenko.kir@gmail.com>
2023-01-04 17:45:39 +0100

    Added fuzz to metals

--- src/materail.hpp
+++ src/materail.hpp
@@ -32,14 +32,15 @@ class lambertian: public material{
 class metal: public material{
     public:
     color albedo;
+    double fuzz;
     metal(){}
-    metal(const color& c):albedo(c){}
+    metal(const color& c, double f):albedo(c), fuzz(f<1 ? f:1){}
 
         virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             vec3 reflected = reflect(normalize(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
